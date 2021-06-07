de14502fcdddda40edb9b8c6b06d00dea719d293
luonghuynh <luonghuynh58227@gmail.com>
2021-06-07 20:53:10 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/crop-image">Crop Image</router-link>
+    <router-link to="/crop-image">Crop Image</router-link> |
+    <router-link to="/search">Search</router-link>
   </div>
   <router-view/>
 </template>
---
+++ src/views/Search.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="filter">
+      <label
+        ><input type="radio" v-model="selectedCategory" value="All" />
+        All</label
+      >
+      <label
+        ><input type="radio" v-model="selectedCategory" value="Tech" />
+        Tech</label
+      >
+      <label
+        ><input type="radio" v-model="selectedCategory" value="Entertainment" />
+        Entertainment</label
+      >
+      <label
+        ><input type="radio" v-model="selectedCategory" value="Fictional" />
+        Fictional</label
+      >
+    </div>
+    <input type="text" v-model="search" placeholder="Search title..." />
+    <ul class="people-list">
+      <li v-for="person in filteredList" :key="person">{{ person.name }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+// import 'vue-js-modal/dist/styles.css'
+
+export default {
+  components: {},
+
+  data() {
+    return {
+      search: "",
+      people: [
+        { name: "Bill Gates", category: "Tech" },
+        { name: "Steve Jobs", category: "Tech" },
+        { name: "Jeff Bezos", category: "Tech" },
+        { name: "George Clooney", category: "Entertainment" },
+        { name: "Meryl Streep", category: "Entertainment" },
+        { name: "Amy Poehler", category: "Entertainment" },
+        { name: "Lady of Lórien", category: "Fictional" },
+        { name: "BB8", category: "Fictional" },
+        { name: "Michael Scott", category: "Fictional" },
+      ],
+      selectedCategory: "All",
+    };
+  },
+
+  methods: {},
+  computed: {
+
+    filteredList() {
+      var vm = this;
+      var category = this.selectedCategory;
+
+      if (category === "All") {
+        return this.people.filter((person) => {
+        return (
+          person.name.toLowerCase().includes(this.search.toLowerCase()) &&
+            this.people
+          );
+        });
+      } else {
+        return this.people.filter((person) => {
+          return (
+            person.name.toLowerCase().includes(this.search.toLowerCase()) &&
+            person.category === category
+          );
+        });
+      }
+    },
+    
+  },
+  mounted() {
+    // this.$modal.show('CropImage');
+  },
+};
+</script>
+
